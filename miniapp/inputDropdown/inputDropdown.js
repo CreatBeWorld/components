9b1124ea5c24935c41ea92a0c1c0d1f1b089cd65
xsj <xsj20@outlook.com>
2023-04-29 17:23:33 +0800

    change

--- miniapp/inputDropdown/inputDropdown.js
+++ miniapp/inputDropdown/inputDropdown.js
@@ -1,52 +1,55 @@
 // components/inputDropdown.js
 Component({
   properties: {
-    // 下拉菜单选项：[{text:'',value:'0'}]
-    // text:菜单的文字，value:选中菜单之后的值      
-    dropDownOptions:{
-      type:Array,
-      value:[]
+    // 下拉菜单选项：['','']
+    // text:菜单的文字，value:选中菜单之后的值
+    dropDownOptions: {
+      type: Array,
+      value: []
     },
     // 菜单标题和选项的选中态颜色
-    activeColor:{
-      type:String,
-      value:'#409eff'
+    activeColor: {
+      type: String,
+      value: '#409eff'
     }
   },
   data: {
-    selectedIndex:0,// 当前选中的下拉菜单项的索引
-    dropDownShow:false,// 下拉菜单项是否显示
-    inputValue:'',// 输入框的内容
+    selectedIndex: 0, // 当前选中的下拉菜单项的索引
+    dropDownShow: false, // 下拉菜单项是否显示
+    inputValue: '' // 输入框的内容
   },
   methods: {
     // 菜单标题被点击
-    dropDownTitleTap(){
+    dropDownTitleTap() {
       this.setData({
-        dropDownShow:!this.data.dropDownShow
+        dropDownShow: !this.data.dropDownShow
       })
     },
     // 菜单选型被点击
-    dropDownItemTap(e){
-      const {index} = e.currentTarget.dataset
-      if(this.data.selectedIndex!==index){
+    dropDownItemTap(e) {
+      const { index } = e.currentTarget.dataset
+      if (this.data.selectedIndex !== index) {
         this.setData({
-          inputValue:''
+          inputValue: ''
         })
       }
       this.setData({
-        selectedIndex:index,
-        dropDownShow:false
+        selectedIndex: index,
+        dropDownShow: false
       })
     },
-    inputHandle(e){
+    inputHandle(e) {
       this.setData({
-        inputValue:e.detail.value
+        inputValue: e.detail.value
       })
     },
     // 点击search按键
-    inputConfirm(){
+    inputConfirm() {
       // index：选中的菜单选项的索引，value:输入框输入的值
-      this.triggerEvent('inputConfirm',{index:this.data.selectedIndex,value:this.data.inputValue})
+      this.triggerEvent('inputConfirm', {
+        index: this.data.selectedIndex,
+        value: this.data.inputValue
+      })
     }
   }
 })
